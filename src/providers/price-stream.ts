ebb36d2dff8679f44638cabe7028be17acd3835b
Reno Raksi <adriano.raksi@gmail.com>
2026-04-13 12:40:58 -0500

    feat: add time parameter to PriceStream tick callback

--- src/providers/price-stream.ts
+++ src/providers/price-stream.ts
@@ -4,11 +4,11 @@ export interface PriceStream {
   subscribe(...symbols: string[]): void;
   unsubscribe(...symbols: string[]): void;
 
-  on(event: 'tick', cb: (symbol: string, price: number) => void): void;
+  on(event: 'tick', cb: (symbol: string, price: number, time: string) => void): void;
   on(event: 'status', cb: (status: StreamStatus) => void): void;
   on(event: 'error', cb: (error: Error) => void): void;
 
-  off(event: 'tick', cb: (symbol: string, price: number) => void): void;
+  off(event: 'tick', cb: (symbol: string, price: number, time: string) => void): void;
   off(event: 'status', cb: (status: StreamStatus) => void): void;
   off(event: 'error', cb: (error: Error) => void): void;
 
